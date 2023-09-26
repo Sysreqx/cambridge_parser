70c14277bf4edc63d2557c791533d1b5298d2888
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-09-26 18:14:22 +0600

    feature(all): collect all in one file, [word + translation + definitions + synonyms]

--- collectTogetherPart2.js
+++ collectTogetherPart2.js
@@ -2,10 +2,8 @@
 const {readFileSync} = require('fs');
 const fs = require("fs");
 
-let file = fs.createWriteStream('wordsCollected.txt');
-
-let filename1 = "wordsWithDefinitions.txt";
-let filename2 = "wordsWithSynonyms.txt";
+let filename1 = "wordsWithTranslation.txt";
+let filename2 = "wordsCollected.txt";
 
 function syncReadFile(filename) {
     const contents = readFileSync(filename, 'utf-8');
@@ -16,12 +14,14 @@ function syncReadFile(filename) {
 let arr1 = syncReadFile(filename1);
 let arr2 = syncReadFile(filename2);
 
+let file = fs.createWriteStream('wordsCollected.txt');
+
 for (let i = 0; i < arr1.length; i++) {
     let flag = true;
     for (let j = 0; j < arr2.length; j++) {
-        if (arr1[i].substring(0, arr1[i].indexOf(' --definitions--')) === arr2[j].substring(0, arr2[j].indexOf(' --synonyms--'))) {
-            console.log(i + ".\t" + arr1[i] + ";" + arr2[j].substring(arr2[j].indexOf(' --synonyms--')));
-            file.write(arr1[i] + ";" + arr2[j].substring(arr2[j].indexOf(' --synonyms--') + 13) + "\n");
+        if (arr1[i].substring(0, arr1[i].indexOf(' - %%%')) === arr2[j].substring(0, arr2[j].indexOf(' --definitions--'))) {
+            console.log(i + ".\t" + arr1[i] + "; (" + arr2[j].substring(arr2[j].indexOf(' --definitions--')));
+            file.write(arr1[i] + "; (" + arr2[j].substring(arr2[j].indexOf(' --definitions--') + 16) + ")\n");
             flag = false;
         }
     }
